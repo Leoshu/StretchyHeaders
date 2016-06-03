42183bff5cb94a1262c428a19ce2ed4e98f3a41e
Leo_hsu <leo_hsu@gemteks.com>
2016-06-03 11:26:38 +0800

    Initial Commit

---
+++ StretchyHeaders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchyHeaders
+//
+//  Created by Leo_hsu on 2016/6/3.
+//  Copyright © 2016年 Leo_hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
